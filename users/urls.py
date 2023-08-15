b374cf8aaecb062ad6cbc1db1f6b975aff42ed29
cassio645 <cantonio645@gmail.com>
2023-08-15 11:56:08 -0300

    login

--- users/urls.py
+++ users/urls.py
@@ -8,20 +8,4 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("cadastro/", views.SignupView.as_view(), name="cadastro"),
-
-    path("password_reset/", 
-        auth_views.PasswordResetView.as_view(template_name="senha/password_reset_form.html"),
-        name="reset_password"),
-
-    path("password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(template_name="senha/password_reset_done.html"),
-        name="password_reset_done"),
-
-    path("reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(template_name="senha/password_reset_confirm.html"),
-        name="password_reset_confirm"),
-
-    path("reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(template_name="senha/password_reset_complete.html"),
-        name="password_reset_complete"),
 ]
